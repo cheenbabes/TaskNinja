e7c8eb99ecd317b596bc3058df87acc6df5cf3ad
Eugene Baibourine <eugene.baibourin@gmail.com>
2015-04-08 10:35:38 -0600

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var app = angular
+  .module('TaskNinjaApp', [
+    'ngAnimate',
+    'ngResource',    
+    'ngRoute',    
+    'firebase'
+  ])
+  .constant('FURL', 'https://popping-fire-7366.firebaseio.com/')  
+  .config(function ($routeProvider) {
+    $routeProvider      
+      .when('/', {
+        templateUrl: 'views/main.html'        
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
